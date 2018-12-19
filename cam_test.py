f72356fc9eb242f211749d32e8702a6a09abaf3a
안우진 <31363862+Andunoid@users.noreply.github.com>
2018-12-19 09:30:14 +0900

    Create cam_test.py

---
+++ cam_test.py
@@ -0,0 +1,13 @@
+import cv2
+cap = cv2.VideoCapture(0) #0 or -1
+while cap.isOpened():
+    ret, img = cap.read()
+    if ret:
+        cv2.imshow('camera-0', img)
+        if cv2.waitKey(1) & 0xFF == 27: #esc
+            break
+    else:
+        print('no camera -0')
+        break
+cap.release()
+cv2.destroyAllWindows()
